a1ae8382279edefa7ba6e12dcb93fa3b2c3d0279
Efthimis Charitonidis <echa@Efthimiss-MacBook-Pro.local>
2023-04-17 19:12:11 +0300

    Handle drop down element propertly by closing it when clicking outside and prevenet loading the same content on each click

--- Client/Shared/Feed/FeedNavView.razor.cs
+++ Client/Shared/Feed/FeedNavView.razor.cs
@@ -1,11 +1,15 @@
 ﻿using Blazorise;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.JSInterop;
 using RssFeeder.Shared.Model;
 
 namespace RssFeeder.Client.Shared.Feed;
 
-public partial class FeedNavView
+public partial class FeedNavView : IDisposable
 {
+    [Inject] IJSRuntime _jsRuntime { get; set; }
+
     [Parameter] public List<FeedNavigation> Model { get; set; }
 
     [Parameter] public EventCallback<FeedNavigation> OnSelectFeedCallback { get; set; }
@@ -14,18 +18,45 @@ public partial class FeedNavView
 
     [Parameter] public EventCallback<FeedNavigation> OnDefaultChangeCallback { get; set; }
 
+    [JSInvokable] public static async Task CloseDropDown() => await OnDomClickEventCallback?.Invoke();
+
     protected FeedNavigation? SelectedFeedItem { get; set; }
 
     private FeedNavOption FeedNavOptionRef;
 
-    private Task OnMoreClickAsync(FeedNavigation item)
+    private static Func<Task> OnDomClickEventCallback;
+
+    public void Dispose()
+    {
+        _jsRuntime.InvokeVoidAsync("unbindDomChanges");
+    }
+
+    protected override async Task OnInitializedAsync()
+    {
+        OnDomClickEventCallback = OnDomClickEventValueAsync;
+
+        await _jsRuntime.InvokeVoidAsync("bindDomChanges");
+    }
+
+    private Task OnDomClickEventValueAsync()
+    {
+        var reference = FeedNavOptionRef.GetRef();
+
+        if (SelectedFeedItem is not null)
+        {
+            SelectedFeedItem = null;
+            reference.Hide();
+        }
+
+        return Task.CompletedTask;
+    }
+
+    private Task OnMoreClickAsync(MouseEventArgs e, FeedNavigation item)
     {
         SelectedFeedItem = item;
 
-        StateHasChanged();
-
         var reference = FeedNavOptionRef.GetRef();
-
+        _jsRuntime.InvokeVoidAsync("updateContextPosition", new { className = "feed-context", x = e.PageX, y = e.PageY });
         return reference.Show();
     }
 
@@ -46,6 +77,9 @@ public partial class FeedNavView
 
     private async Task OnFeedNavClickAsync(FeedNavigation item)
     {
+        // If it's already selected skip
+        if (Model.Single(x => x.Id == item.Id).Active) return;
+
         Model.ForEach(i => i.Active = false);
         item.Active = true;
 
---
+++ Client/wwwroot/js/context.js
@@ -0,0 +1,20 @@
+﻿function updateContextPosition(op) {
+    const element = document.querySelector(`.${op.className}`);
+    element.style.left = `${op.x}px`;
+    element.style.top = `${op.y}px`;
+}
+
+function bindDomChanges() {
+    console.log("binded");
+    document.addEventListener('click', function (event) {
+        if (!event.target.classList.contains('nav-icon')) {
+            DotNet.invokeMethodAsync('RssFeeder.Client', 'CloseDropDown');
+        }
+    });
+}
+
+function unbindDomChanges() {
+    console.log("unbinded");
+    document.removeEventListener('click', function (event) {
+    });
+}
