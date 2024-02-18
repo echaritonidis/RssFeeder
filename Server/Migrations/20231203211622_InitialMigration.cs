42b84926c0b7ac00d699c5bd410baafd99ddfbec
echaritonidis <efthimis@charitonidis.xyz>
2024-02-18 19:32:52 +0200

    Bump nugets

---
+++ Server/Migrations/20231203211622_InitialMigration.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RssFeeder.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FeedGroup",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Title = table.Column<string>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: true),
+                    Color = table.Column<string>(type: "TEXT", nullable: false),
+                    Order = table.Column<int>(type: "INTEGER", nullable: false),
+                    Initial = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FeedGroup", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Settings",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    DarkMode = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Settings", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Feed",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    ModifiedAt = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Title = table.Column<string>(type: "TEXT", nullable: false),
+                    Href = table.Column<string>(type: "TEXT", nullable: false),
+                    Favorite = table.Column<bool>(type: "INTEGER", nullable: false),
+                    Default = table.Column<bool>(type: "INTEGER", nullable: false),
+                    FeedGroupId = table.Column<Guid>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Feed", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Feed_FeedGroup_FeedGroupId",
+                        column: x => x.FeedGroupId,
+                        principalTable: "FeedGroup",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Label",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: true),
+                    Color = table.Column<string>(type: "TEXT", nullable: true),
+                    FeedId = table.Column<Guid>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Label", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Label_Feed_FeedId",
+                        column: x => x.FeedId,
+                        principalTable: "Feed",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "FeedGroup",
+                columns: new[] { "Id", "Color", "Description", "Initial", "Order", "Title" },
+                values: new object[] { new Guid("f5de41b1-89c4-488a-9bad-f92231fdfc33"), "#000", "Generic specific category or topic", true, 0, "Unclassified" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Feed_FeedGroupId",
+                table: "Feed",
+                column: "FeedGroupId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Label_FeedId",
+                table: "Label",
+                column: "FeedId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Label");
+
+            migrationBuilder.DropTable(
+                name: "Settings");
+
+            migrationBuilder.DropTable(
+                name: "Feed");
+
+            migrationBuilder.DropTable(
+                name: "FeedGroup");
+        }
+    }
+}
